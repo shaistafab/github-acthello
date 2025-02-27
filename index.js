c0c8d5eb996717dd780eb4bddf85dfb0c71913c4
shaistafab <shaistaafreen10.sa@gmail.com>
2025-02-27 20:31:43 +0000

    Initial version

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import * as core from '@actions/core';
+import * as core from '@actions/github';
 
-const name = core.getInput('name')
-const output_value = 'Hello ${name} from GITHUB-ACTHELLO!';
-core.setOutput('greeting', output_value)
+
+try{
+    const name = core.getInput('name')
+    const output_value = 'Hello ${name} from GITHUB-ACTHELLO!';
+    core.setOutput('greeting', output_value)
+}catch (error) {
+    core.setFailed(error.message);
+}
+
